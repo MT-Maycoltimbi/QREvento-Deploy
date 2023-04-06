f9b3827fd07a965c6a7c37e598067270a37a9fed
MICHAEL RONALD TIMBI QUISPILEMA <mtimbiq@unemi.edu.ec>
2023-04-06 14:57:50 -0500

    update 10

--- build.sh
+++ build.sh
@@ -3,7 +3,37 @@
 set -o errexit
 
 # poetry install
-pip install -r requirements.txt
+pip install asgiref
+pip instal autopep8
+pip instal Brotli
+pip instal colorama
+pip instal contourpy
+pip instal cycler
+pip instal dj-database-url
+pip instal Django
+pip instal fonttools
+pip instal gunicorn
+pip instal kiwisolver
+pip instal matplotlib
+pip instal numpy
+pip instal opencv-contrib-python
+pip instal opencv-python
+pip instal packaging
+pip instal Pillow
+
+pip instal psycopg2-binary
+pip instal pycodestyle
+pip instal pyparsing
+pip instal pypng
+python-dateutil
+pip instal pyzbar
+pip instal qrcode
+pip instal six
+pip instal sqlparse
+pip instal typing_extensions
+pip instal tzdata
+pip instal whitenoise
+
 
 
 
